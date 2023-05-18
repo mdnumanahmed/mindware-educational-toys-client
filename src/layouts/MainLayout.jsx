08584c52f77cb2c69726c9c2fffaef6c27ed9c52
mdnumanahmed <mnumansyl@gmail.com>
2023-05-18 09:47:10 +0600

    footer added

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import MindWareNavbar from "../shared/MindWareNavbar";
+import MindWareFooter from "../shared/MindWareFooter";
 
 const MainLayout = () => {
     return (
         <div>
             <MindWareNavbar />
             <Outlet />
+            <MindWareFooter />
         </div>
     );
 };
